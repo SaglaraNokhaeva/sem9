335ce88d56d70b29330e049ba8fe2274585ef413
Saglara <nohaevass@yandex.ru>
2023-08-19 09:44:57 +0300

    1

--- ex4.py
+++ ex4.py
@@ -2,3 +2,20 @@
 # Создайте декоратор с параметром.
 # Параметр - целое число, количество запусков декорируемой функции.
 
+def call_count(num):
+    def decorator(func):
+        result = []
+        def wrapper(*args, **kwargs):
+            for _ in range(num):
+                result.append(func(*args, **kwargs))
+            return result
+        return wrapper
+    return decorator
+
+
+@call_count(5)
+def printer(string):
+    print(string)
+    return 'ok'
+
+print(printer('сработало!'))
