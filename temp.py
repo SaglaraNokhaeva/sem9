83e4242796a18f7ef6f7c9d9b53928a32d3f71d8
Saglara <nohaevass@yandex.ru>
2023-08-19 20:37:58 +0300

    1

--- temp.py
+++ temp.py
@@ -2,19 +2,22 @@ import math
 
 
 def quadratic_equation(a, b, c):
+    answer = []
     discr = b ** 2 - 4 * a * c
-    if discr > 0:
-        x1 = (-b + math.sqrt(discr)) / (2 * a)
-        x2 = (-b - math.sqrt(discr)) / (2 * a)
-        print(x1, x2)
-        return x1, x2
-    elif discr == 0:
-        x = -b / (2 * a)
-        print(x)
-        return x
+    if a==0:
+        x = -c/b
+        answer.append(x)
     else:
-        print("Корней нет")
-        return "Корней нет"
-quadratic_equation(-16.0, -14.0, 53.0)
-
+        if discr > 0:
+            x1 = (-b + math.sqrt(discr)) / (2 * a)
+            x2 = (-b - math.sqrt(discr)) / (2 * a)
+            answer.append(x1)
+            answer.append(x2)
+        elif discr == 0:
+            x = -b / (2 * a)
+            answer.append(x)
+        else:
+            answer.append("Корней нет")
+    return answer
+print(quadratic_equation(0,-2,1))
 # -2.309372391483992, 1.434372391483992
