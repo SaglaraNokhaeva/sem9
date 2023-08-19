a33266d7adfd532afa60277d4b2f431de2892a05
Saglara <nohaevass@yandex.ru>
2023-08-19 09:30:39 +0300

    1

--- ex3.py
+++ ex3.py
@@ -7,6 +7,24 @@
 # json словаря.
 # Для декорирования напишите функцию, которая может
 # принимать как позиционные, так и ключевые аргументы.
-# Имя файла должно совпадать с именем декорируемой
-# функции.
+# Имя файла должно совпадать с именем декорируемой функции.
+import json
 
+
+def json_saver(func):
+    def wrapper(*args, **kwargs):
+        with open(f'{func.__name__}.json', 'a') as file:
+            temp_dict = {'args' : args}
+            temp_dict.update(kwargs)
+            result = func(*args, **kwargs)
+            temp_dict['result'] = result
+            json.dump(temp_dict, file, indent=3, ensure_ascii=False)
+        return result
+    return wrapper
+
+
+@json_saver
+def example(a, b, c = 5):
+    return max(a, b, c)
+
+print(example(1, 14))
